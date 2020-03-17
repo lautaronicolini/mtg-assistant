aeb29772d18bca5716fd58ea9ed7b3ffe1e11ecb
Lautaro Nicolini <lautaronnicolini@gmail.com>
2020-03-17 10:09:11 -0300

    Primer consumo de API scryfall

--- mtg-assistant/src/App.js
+++ mtg-assistant/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from "react"
 import './App.css';
+
+class App extends Component {
+  state = {
+    cardImage: ""
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input id="cardName" type="text"></input>
+      <button id="searchCard" onclick="searchCard()">Buscar</button>
     </div>
   );
+}
+
+function searchCard(){
+  fetch('https://api.scryfall.com/cards/search',{
+    method: 'GET',
+    body: JSON.stringify({
+      q: document.getElementById('cardName').nodeValue
+    })
+  })
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ cardImage: data })
+        })
+        .catch(console.log)
+
 }
 
 export default App;
